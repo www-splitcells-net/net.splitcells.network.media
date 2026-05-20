74219dcfaa9a3292f1cefe5ee78160078597d528
Mārtiņš Avots <martins.avots@splitcells.net>
2026-05-20 21:57:21 +0200

    Committed by "splitcells" on "splitcells-XPS-15-9570".

--- src/main/java/net/splitcells/network/media/NetworkMediaFileSystem.java
+++ src/main/java/net/splitcells/network/media/NetworkMediaFileSystem.java
@@ -16,12 +16,19 @@
 package net.splitcells.network.media;
 
 import net.splitcells.dem.environment.config.framework.OptionImpl;
+import net.splitcells.dem.lang.tree.Tree;
 import net.splitcells.dem.resource.FileSystemView;
 
+import java.util.Optional;
+
+import static net.splitcells.dem.lang.tree.TreeI.tree;
 import static net.splitcells.dem.resource.FileSystemViaClassResources.fileSystemViaClassResources;
 
 public class NetworkMediaFileSystem extends OptionImpl<FileSystemView> {
     public NetworkMediaFileSystem() {
         super(() -> fileSystemViaClassResources(NetworkMediaFileSystem.class, "net.splitcells", "network.media"));
+    }
+    @Override public Optional<Tree> serialize(FileSystemView currentValue) {
+        return Optional.of(tree(currentValue.toString()));
     }
 }
